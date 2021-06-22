606bceb38a2418cce4d217476b2e1995c3bc88e9
Bekmamatova <aidanekbekmamatova@gmail.com>
2021-06-22 15:22:01 +0600

    added static root

--- fullstackproject/settings.py
+++ fullstackproject/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from decouple import config
 from pathlib import Path
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
